a7d36b52c6e59b40382164a3b01bc04e4ea65a6a
Tobias Lütke <tobi@shopify.com>
2011-12-23 13:37:10 -0500

    reformat

--- main.go
+++ main.go
@@ -1,125 +1,123 @@
 package main
 
 import (
-  "io"
-  "os"
-  "fmt"
-  "bufio"
-  "exec"
-  "http"
-  "time"
-  "flag"
-  "log"
-  "net"
-  "strconv"
+	"io"
+	"os"
+	"fmt"
+	"bufio"
+	"exec"
+	"http"
+	"time"
+	"flag"
+	"log"
+	"net"
+	"strconv"
 )
 
 var port *int = flag.Int("port", 3333, "port")
 var executable *string = flag.String("executable", "server", "executable to launch. Must support -pXXXX for port assignment")
 var compiler *string = flag.String("compiler", "make", "compiler to execute. make by default.")
 
-
 func error(client io.WriteCloser, s string) {
-  log.Printf("Error: %s", s)
-  client.Write([]byte("HTTP/1.0 500 Internal Error\n"))
-  client.Write([]byte("Content-Type: text/plain\n"))
-  client.Write([]byte("\n\n"+s+"\n"))
-  client.Close()
+	log.Printf("Error: %s", s)
+	client.Write([]byte("HTTP/1.0 500 Internal Error\n"))
+	client.Write([]byte("Content-Type: text/plain\n"))
+	client.Write([]byte("\n\n" + s + "\n"))
+	client.Close()
 }
 
 func pipeRequestResponse(server, client net.Conn) os.Error {
-  
-  // Read request from wire
-  req, err := http.ReadRequest(bufio.NewReader(client))
-  if err != nil {
-    return err
-  }
 
-  rawReq, err := http.DumpRequest(req, true)
-  if err != nil {
-    return err
-  }
+	// Read request from wire
+	req, err := http.ReadRequest(bufio.NewReader(client))
+	if err != nil {
+		return err
+	}
 
-  // forward it on
-  server.Write(rawReq)
+	rawReq, err := http.DumpRequest(req, true)
+	if err != nil {
+		return err
+	}
 
-  // Read response from wire
-  resp, err := http.ReadResponse(bufio.NewReader(server), req)
-  if err != nil {
-    return err
-  }
+	// forward it on
+	server.Write(rawReq)
 
-  rawResp, err := http.DumpResponse(resp, true)
-  if err != nil {
-    return err
-  }
+	// Read response from wire
+	resp, err := http.ReadResponse(bufio.NewReader(server), req)
+	if err != nil {
+		return err
+	}
 
-  // forward it on
-  client.Write(rawResp)
-  return nil
+	rawResp, err := http.DumpResponse(resp, true)
+	if err != nil {
+		return err
+	}
+
+	// forward it on
+	client.Write(rawResp)
+	return nil
 }
 
 func forward(client net.Conn) {
-  defer client.Close()
+	defer client.Close()
 
-  cmd := exec.Command(*compiler)
-  output, err := cmd.CombinedOutput()
+	cmd := exec.Command(*compiler)
+	output, err := cmd.CombinedOutput()
 
-       
-  if err != nil { 
-    error(client, string(output))
-    return
-  }    
+	if err != nil {
+		error(client, string(output))
+		return
+	}
 
-  cmd = exec.Command(*executable, "-p", strconv.Itoa(*port+1024))
-  err = cmd.Start()
-     
-  if err != nil { 
-    error(client, err.String())
-    return
-  }    
+	cmd = exec.Command(*executable, "-p", strconv.Itoa(*port+1024))
+	err = cmd.Start()
 
-  // 20 retries, ~= 10 secs of attempts
-  for i := 0; i < 20; i++ {
-    server, err := net.Dial("tcp", "127.0.0.1:5050")    
-    if err == nil {
+	if err != nil {
+		error(client, err.String())
+		return
+	}
 
-      err = pipeRequestResponse(server, client)
-      if err != nil {
-        error(client, err.String())
-        return
-      }
+	// 20 retries, ~= 10 secs of attempts
+	for i := 0; i < 20; i++ {
+		server, err := net.Dial("tcp", "127.0.0.1:5050")
+		if err == nil {
 
-      log.Print("Forwarded...")  
+			err = pipeRequestResponse(server, client)
+			if err != nil {
+				error(client, err.String())
+				return
+			}
 
-      return
-    }
+			log.Print("Forwarded...")
 
-    time.Sleep(5e8) // 500ms
-  }
+			return
+		}
 
-  error(client, err.String())    
+		time.Sleep(5e8) // 500ms
+	}
+
+	error(client, err.String())
 }
 
 func main() {
-  flag.Parse()
- 	
-	server, err := net.Listen("tcp", ":"+ strconv.Itoa(*port))  
+	flag.Parse()
+
+	server, err := net.Listen("tcp", ":"+strconv.Itoa(*port))
 	if err != nil {
 		log.Fatal("Listen: ", err.String())
 	}
 
-  fmt.Printf("Listening on port %d\n", *port)
-  fmt.Printf(" -> forwarding to executable %s -p%d\n", *executable, *port+1024)
+	fmt.Printf("Listening on port %d\n", *port)
+	fmt.Printf(" -> forwarding to executable %s -p%d\n", *executable, *port+1024)
 
-  for {
-     conn, err := server.Accept()
-    
-     if err != nil {
-       log.Fatal("accept:", err.String()) // TODO(r): exit?
-     }
+	for {
+		conn, err := server.Accept()
 
-     go forward(conn)    
-   }
+		if err != nil {
+			log.Fatal("accept:", err.String()) // TODO(r): exit?
+		}
+
+		go forward(conn)
+	}
 
 }
